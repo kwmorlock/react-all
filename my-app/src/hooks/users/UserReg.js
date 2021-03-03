459578ac3287fdf4745383595d16b2ed301757e3
kwmorlock <kmorlockcodes@gmail.com>
2021-03-03 10:39:55 -0500

    fix: UserReg.js axiosWithAuth

--- my-app/src/hooks/users/UserReg.js
+++ my-app/src/hooks/users/UserReg.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import * as yup from "yup";
-import { axiosWithAuth } from "../../utils/axiosWithAuth";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Register = (props) => {
   const [userData, setUserData] = useState({
