b36849196152f3c0fac57ad4e9d7653ee8db4471
kwmorlock <kmorlockcodes@gmail.com>
2021-03-03 11:05:59 -0500

    set: App.js login path

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
 import Register from "./hooks/users/UserReg";
+import Login from "./hooks/users/UserLogin";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
         </Switch>
       </div>
     </Router>
