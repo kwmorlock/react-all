e44c8c2769adc1564722208f778adb6dab0a4dfa
kwmorlock <kmorlockcodes@gmail.com>
2021-03-03 10:38:22 -0500

    set: App.js register path

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
+import React from "react";
 import "./App.css";
+import Register from "./hooks/users/UserReg";
 
 function App() {
   return (
-    <div className="App">
-      <p>setup</p>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/register" component={Register} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
